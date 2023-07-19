17aab7cb13612b58096ece167c55c88c68b3bdbe
BinoB <binob831@gmail.com>
2023-07-19 22:16:35 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose")
+
+
+const app = express();
+app.use(express.json());
+const itemsRoute = require("./routes/itemsRoute");
+const usersRoute = require("./routes/userRoute");
+const billsRoute = require('./routes/billsRoute')
+app.use("/api/items/", itemsRoute);
+app.use("/api/users/", usersRoute);
+app.use("/api/bills/", billsRoute);
+const path = require('path');
+const { log } = require("console");
+
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server running on PORT ${PORT}`)
+    })
+}).catch((error)=>{
+    console.log(error)
+})
+
+
+
+
+
+app.get("/", (req, res) => res.send("Hello World! from home api"));
